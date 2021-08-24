3edaa0bb22346b104bc8db4feaa9af5afcfb82e4
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2021-08-24 17:28:39 +0200

    minimal import from git

--- test_flavours/flavours/git/flavour.nix
+++ test_flavours/flavours/git/flavour.nix
@@ -1,21 +1,7 @@
 let
-  name = "test-git";
   src = builtins.fetchGit {
     url = "https://github.com/GuilloteauQ/test-flavour.git";
     rev = "5998f33b99e90e314a149fb092529950b383bf37";
   };
-  driver = src + "/driver.py";
-  description = "test git driver";
 in
-{
-  inherit name driver description;
-  compose = { pkgs ? import <nixpkgs> {}, ... }:
-
-  pkgs.writeTextFile {
-    name = "compose-info.json";
-    text = builtins.toJSON({
-      inherit driver src;
-      flavour = name;
-    });
-  };
-}
+  import (src + "/flavour.nix")
